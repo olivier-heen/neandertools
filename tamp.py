bdb23e854e8c1ebefc7fd81d73dbf97254e90b3d
olivier-heen <86301971+olivier-heen@users.noreply.github.com>
2021-06-26 03:49:23 +0200

    pylint3 ok, flake8 ok.

--- tamp.py
+++ tamp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-################################################################v###############
+# ##############################################################v##############
 '''Convert successive numbers to intervals. And vice-versa.
 
 Usage:
@@ -13,22 +13,60 @@ Examples:
     tamp.py < vlan.csv > addr.csv
     diff <(tamp.py test | tamp.py) sample  &&  echo "Involution."
 '''
-################################################################v###############
+# ##############################################################v##############
 
-from sys    import stdin    as stdi
-from docopt import docopt   as args
+from sys import stdin as stdi
+from docopt import docopt as args
 
-def show(init, stop):
-    '''Print the interval "[init, stop]" or just "init" if bounds are equal.'''
+
+def show_interval(init, stop):
+    '''Print "init-stop" or just "init" if bounds are equal.'''
     if init == stop:
         print(init)
     else:
         print(init, stop, sep=SEPA)
 
-################################################################v###############
+
+def boom(val1, val2):
+    '''Explode interval "val1-val2" as a number range.'''
+    return boom(val2, val1) if val1 > val2 else range(val1, val2+1)
+
+
+def conv():
+    '''Main conversion loop.'''
+    init = curr = look = None
+
+    for read in stdi:
+        line = read.strip()                                     # Line by line
+
+        try:
+            look = int(line)                                    # It's an int?
+            if init is None:                                    # Start interv.
+                init = curr = look
+            elif look == curr+1:                                # Grow interv.
+                curr = look
+            else:                                               # Stop interv.
+                show_interval(init, curr)                       # Show interv.
+                init = curr = look                              # Start another
+
+        except ValueError:                                      # Not an int
+
+            if init is not None:                                # Must flush?
+                show_interval(init, curr)
+                init = curr = look = None
+            try:                                                # Interval?
+                val1, val2 = line.split(SEPA)
+                for step in boom(int(val1), int(val2)):         # Explode
+                    print(step)
+            except (ValueError, TypeError):                     # Unknown
+                print(line)                                     # Leab as is
+
+    if init is not None:                                        # Degenerate
+        show_interval(init, curr)                               # Just show
+
+
+# ##############################################################v###############
 if __name__ == '__main__':
-    INIT = CURR = NEXT = None
-    INCL = lambda x, y: INCL(y, x) if x > y else range(x, y+1)  # Include bounds
     ARGS = args(__doc__)
     SEPA = ARGS['-d']
     if len(SEPA) > 1:
@@ -36,42 +74,16 @@ if __name__ == '__main__':
     if SEPA in '.1234567890':
         quit('tamp.py: the interval delimiter cannot be a dot or a digit.')
 
-    for l in stdi:
-        line = l.strip()                                        # Line by line
+    conv()
 
-        try:
-            NEXT = int(line)                                    # It's an int?
-            if INIT is None:                                    # Start interval
-                INIT = CURR = NEXT
-            elif NEXT == CURR+1:                                # Grow interval
-                CURR = NEXT
-            else:                                               # Stop interval
-                show(INIT, CURR)                                # Show interval
-                INIT = CURR = NEXT                              # Start another
-
-        except ValueError:                                      # It's interval?
-
-            if INIT is not None:                                # Ongoing list?
-                show(INIT, CURR)                                # Show list
-                INIT = CURR = NEXT = None                       # Stop list
-            try:                                                # tamp interval?
-
-                for step in INCL(*(int(i) for i in line.split(SEPA))):
-                    print(step)
-            except (ValueError, TypeError):                     # Can't tamp?
-                print(line)                                     # Don't change
-
-    if INIT is not None:                                        # Degenerate
-        show(INIT, CURR)                                        # Just show
-
-################################################################v###############
-#   This code is meant to fit on 79 columns according to PEP 8. #<-64      79->#
-#   I like to use the 64 first columns for code and the last 15 # for tiny notes
-################################################################^###############
-#MUST
+# ##############################################################v##############
+#   This code is meant to fit on 79 columns according to PEP 8. #<-64     79->#
+#   I like to use the 64 first columns for code and the last 15 # for tiny note
+# ##############################################################^##############
+# MUST
 #   --mini      Reduce the lists but do not expand the intervals.
 #   --maxi      Expand the intervals but do not reduce the lists.
-################################################################|###############
-#NICE
+# ##############################################################|##############
+# NICE
 #   Process negative numbers in intervals: -23--37  -37--23  11--13  -11-13.
-################################################################^###############
+# ##############################################################^##############
