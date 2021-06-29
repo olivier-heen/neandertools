fdec9454d1e4c28ffba79fa65cd7960d157e3170
olivier-heen <86301971+olivier-heen@users.noreply.github.com>
2021-06-29 13:36:38 +0200

    Added norm.sh (using cidr.py iton.py cidr.py).

---
+++ norm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# ##############################################################v###############
+HELP="
+Usage   norm.sh <FILE>
+FILE    List of cidr, intervals, addresses.
+Result  Equivalent list of aligned cidr, merged and deduplicated.
+"
+
+FILE="${1}"
+[[ ! -f "${FILE}" ]] && echo "${HELP}" && exit 1
+
+(
+./cidr.py -i	|\
+./iton.py	|\
+./tamp.py	|\
+sort -un	|\
+./tamp.py	|\
+./iton.py	|\
+./cidr.py	|\
+sort -uV
+)	<\
+"${FILE}"
