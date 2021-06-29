88bcbd46b893bc638a2a610e12290101d789502b
olivier-heen <86301971+olivier-heen@users.noreply.github.com>
2021-06-29 11:34:03 +0200

    Added -c and -i options.

--- cidr.py
+++ cidr.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-'''
 # ##############################################################v##############
-Convert cidr network range to addresse interval.  And vice-versa.
+'''
+Convert a cidr network to an address interval.    And vice-versa.
 
 Usage:
-    cidr.py [-d <sep>] [--cidr | --range] [ITEM]
+    cidr.py [-d <sep>] [-c | -i] [ITEM]
 
 options:
     -d <sep>    One character interval separator.    [default: -]
-    -c --cidr   Just convert ranges to cidr. (todo)
-    -r --range  Just convert cidr to ranges. (todo)
+    -c          Leave cidr as is.
+    -i          Leave interval as is.
 
 Example:
     cidr.py 192.168.0.4-192.168.0.12
@@ -18,8 +18,8 @@ Example:
     192.168.0.12/32
 
 Remarks:
-    Bad cidr are approximated to the closest range.
-    There is no range aggregation.
+    Bad cidr are approximated to the closest interval.
+    Consecutive intervals are not aggregated.
 '''
 
 from sys import stdin as stdi
@@ -35,21 +35,27 @@ def conv(line):
         init = ipaddress.IPv4Address(init.strip())
         stop = ipaddress.IPv4Address(stop.strip())
         cidr = ipaddress.summarize_address_range(init, stop)
+        if INTL_ASIS:
+            return line
         return '\n'.join(str(addr) for addr in cidr)
     except ValueError:
         try:                                                    # CIDR?
             cidr = ipaddress.ip_network(line, strict=False)
             init = str(cidr[0])
             stop = str(cidr[-1])
+            if CIDR_ASIS:
+                return line
             return init + SEPA + stop
         except (ValueError, TypeError):                         # Unknown
             return line
 
-        
+
 # ##############################################################v##############
 if __name__ == "__main__":
     ARGS = args(__doc__)
     ITEM = ARGS['ITEM']
+    CIDR_ASIS = ARGS['-c']
+    INTL_ASIS = ARGS['-i']
     SEPA = ARGS['-d']
     if len(SEPA) > 1:
         quit('cidr.py: the interval delimiter must be a single character.')
